2fa6a56af56e0c05289b73bf67362dcc718ba09b
maciejjablonsky <maciekjablonsky@gmail.com>
2022-11-13 22:38:00 +0100

    slide02

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,10 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private int sekundy = 0;
+    private boolean realizowany = false;
+    private boolean byl_uruchomiony = false;
 
     @Override
     protected void onCreate(Bundle paczka) {
         super.onCreate(paczka);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle paczka) {
+        super.onSaveInstanceState(paczka);
+        paczka.putInt("sekundy", sekundy);
+        paczka.putBoolean("realizacja", realizowany);
+        paczka.putBoolean("poprzednio", byl_uruchomiony);
     }
 }
